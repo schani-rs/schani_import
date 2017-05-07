bcf94f4d6131cd6cad660c2f13ad097d73e5160a
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-05-07 13:12:45 +0200

    Refactor modules into indep crates
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![feature(plugin, custom_derive)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use rocket::Data;
+use rocket::request::Form;
+
+#[derive(FromForm)]
+struct Import {
+    name: String,
+}
+
+#[post("/upload", data = "<import>")]
+fn upload_data(import: Form<Import>) -> String {
+    let imported_file = import.get();
+
+    // TODO: save data to local DB
+
+    format!("file {} saved", imported_file.name)
+}
+
+#[post("/upload/<file_id>", data = "<data>")]
+fn upload_image(file_id: &str, data: Data) -> String {
+    print!("uploading image {}", file_id);
+
+    // TODO: check if `file_id` is valid
+    // TODO: transfer file to the store
+    // TODO: prevent directory traversals
+    // TODO: save extension/type on data import
+    data.stream_to_file(format!("/tmp/{}.NEF", file_id));
+
+    format!("image {} uploaded successfully", file_id)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![upload_image, upload_data])
+        .launch();
+}
