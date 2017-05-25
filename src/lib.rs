d992cacc48a937084d7711d246203579a9e86ca3
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-05-25 09:11:00 +0200

    Return proper JSON response when importing images
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_codegen;
 extern crate dotenv;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
 
 pub mod models;
 pub mod schema;
